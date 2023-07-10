24f1688fc178e5a9208dbacf5b9e91168fe803ba
TaeHo Kim <tae1094@gmail.com>
2023-07-10 22:59:44 +0900

    update nc_dashboard_draft3.py file and add requirements.txt file

--- nc_dashboard/nc_dashboard_draft3.py
+++ nc_dashboard/nc_dashboard_draft3.py
@@ -6,7 +6,6 @@ import streamlit as st
 
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
