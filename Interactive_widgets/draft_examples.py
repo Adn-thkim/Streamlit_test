bfa88156a66f10292f6d0a981d2de98201b89d9f
TaeHo Kim <tae1094@gmail.com>
2023-07-10 22:40:25 +0900

    update draft_examples.py file

--- Interactive_widgets/draft_examples.py
+++ Interactive_widgets/draft_examples.py
@@ -42,6 +42,8 @@ st.write(f'입력된 비밀번호는 {pw}입니다')
 
 
 st.title('Text Area')
+st.write('')
+st.write('')
 
 st.text_area(label='문의 내용을 입력해주세요', help='영업시간 외 작성된 문의에 대한 답변은 익일 영업시간에 등록될 예정입니다.', key='1')
 st.text_area(label='문의 내용을 입력해주세요', height=200, max_chars=1000, key='2')
@@ -52,8 +54,8 @@ st.text_area(label='문의 내용을 입력해주세요', height=200, max_chars=
 
 st.title('Number Input')
 
-st.number_input('나이를 입력해주세요.', min_value=0, step=1, key='4')
-st.number_input('나이를 입력해주세요.', value=20, step=1, key='5')
-st.number_input('나이를 입력해주세요.', step=0.1, format='%.1f', key='6')
+st.number_input('나이를 입력해주세요.', min_value=0, value=20, step=1, key='4')
+st.number_input('키를 입력해주세요.', min_value=0, value=170, step=1, key='5')
+st.number_input('몸무게를 입력해주세요.', min_value=0.0, value=50.00, step=0.1, format='%.2f', key='6')
 
 
